80c04f78793c9dc7c85a54a30a0988acb0f984b2
GaikwadAnil <anilgaikwad2298@gmail.com>
2025-04-02 15:51:50 +0530

    email_sender.py

---
+++ app/utils/email_sender.py
@@ -0,0 +1,28 @@
+import smtplib
+from email.message import EmailMessage
+from app.config import EMAIL_HOST, EMAIL_PORT, EMAIL_USERNAME, EMAIL_PASSWORD
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def send_email(to_email: str, subject: str, body: str):
+    try:
+        msg = EmailMessage()
+        msg["Subject"] = subject
+        msg["From"] = EMAIL_USERNAME
+        msg["To"] = to_email
+        msg.set_content(body)
+
+        with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+            server.send_message(msg)
+        logger.info(f"Email sent successfully to {to_email}")
+
+    except smtplib.SMTPConnectError:
+        logger.error("Failed to connect to the SMTP server.")
+    except smtplib.SMTPAuthenticationError:
+        logger.error("SMTP authentication failed.")
+    except smtplib.SMTPException as e:
+        logger.error(f"SMTP error occurred: {e}")
