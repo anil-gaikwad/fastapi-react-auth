06b3606c839c402cb0ebb65d3687e6476f973904
GaikwadAnil <anilgaikwad2298@gmail.com>
2025-04-18 18:41:46 +0530

    update models

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_verified = Column(Boolean, default=False)
 
